0c99cf4f0a26b11888b3a447531364ce610dccb5
GeorgeIlincuta <GeorgeIlincuta@users.noreply.github.com>
2022-09-14 08:40:31 +0300

    Added Popular and Category

--- EcomXamarin/EcomXamarin/Pages/HomePage.xaml.cs
+++ EcomXamarin/EcomXamarin/Pages/HomePage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using EcomXamarin.Models;
+using EcomXamarin.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,47 @@ namespace EcomXamarin.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        public ObservableCollection<PopularProduct> ProductsCollection;
+        public ObservableCollection<Category> CategoriesCollection;
         public HomePage()
         {
             InitializeComponent();
+            ProductsCollection = new ObservableCollection<PopularProduct>();
+            CategoriesCollection = new ObservableCollection<Category>();
+            GetPopularProducts();
+            GetCategories();
+        }
+
+        private async void GetCategories()
+        {
+            var categories = await ApiService.GetCategories();
+            foreach (var category in categories)
+            {
+                CategoriesCollection.Add(category);
+            }
+            CvCategories.ItemsSource = CategoriesCollection;
+        }
+
+        private async void GetPopularProducts()
+        {
+            var products = await ApiService.GetPopularProducts();
+            foreach (var product in products)
+            {
+                ProductsCollection.Add(product);
+            }
+            CvProducts.ItemsSource = ProductsCollection;
+        }
+
+        private async void ImgMenu_Tapped(object sender, EventArgs e)
+        {
+            GridOverlay.IsVisible = true;
+            await SlMenu.TranslateTo(0,0,400,Easing.Linear);
+        }
+
+        private async void TapCloseMenu_Tapped(object sender, EventArgs e)
+        {
+            await SlMenu.TranslateTo(-250, 0, 400, Easing.Linear);
+            GridOverlay.IsVisible = false;
         }
     }
 }
